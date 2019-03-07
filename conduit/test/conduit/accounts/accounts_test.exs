39872f2ed47c8965814fa3a9e72c210ca1532e1d
José Pamplona <pamplona.stoever@gmail.com>
2019-03-07 16:11:20 -0600

    Register user, returning registered user projection

--- conduit/test/conduit/accounts/accounts_test.exs
+++ conduit/test/conduit/accounts/accounts_test.exs
@@ -2,18 +2,18 @@ defmodule Conduit.AccountsTest do
   use Conduit.DataCase
 
   alias Conduit.Accounts
-  alias Conduit.Accounts.Aggregates.User
+  alias Conduit.Accounts.Projections.User
 
   describe "register user" do
     @tag :integration
     test "should succeed with valid data" do
       assert {:ok, %User{} = user} = Accounts.register_user(build(:user))
 
-      assert user.bio == "some bio"
-      assert user.email == "some email"
-      assert user.pass_hash == "some hashed_password"
-      assert user.image == "some image"
-      assert user.username == "some username"
+      assert user.username == "jake"
+      assert user.email == "jake@jake.jake"
+      assert user.pass_hash == "jakejake"
+      assert user.bio == nil
+      assert user.image == nil
     end
 
     @tag :integration
